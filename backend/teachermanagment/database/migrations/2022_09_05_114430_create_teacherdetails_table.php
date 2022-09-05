b8e73028081d48d0f2bb079cbf6779ebcca08b76
sajidkhan56 <sajid.web56@gmail.com>
2022-09-05 17:00:28 +0500

    database table, models, and controllers added

---
+++ backend/teachermanagment/database/migrations/2022_09_05_114430_create_teacherdetails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeacherdetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacherdetails', function (Blueprint $table) {
+            $table->id('teacherdetails_id');
+            $table->string('name');
+            $table->string('qualification');
+            $table->time('start_timing');
+            $table->time('end_timing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacherdetails');
+    }
+}
